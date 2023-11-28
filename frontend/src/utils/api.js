65aed8a373ccdfae2bd1a14a4c4d7c0f3ae7108e
WhatsAppIT <it.technology@internet.ru>
2023-11-28 22:33:57 +0400

    asdsdasdasd

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -105,7 +105,7 @@ class Api {
 const api = new Api({
     url: 'http://api.krivolapov.nomoredomainsmonster.ru',
     headers: {
-        //authorization: 'dc5ffe71-d4a2-4ae4-8d8f-113b04708a8c',
+        authorization: 'dc5ffe71-d4a2-4ae4-8d8f-113b04708a8c',
         'Content-type': 'application/json',
     },
 });
