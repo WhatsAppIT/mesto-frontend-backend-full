b950b36315fbbea77e96c7a1bc95b7181803aeac
WhatsAppIT <it.technology@internet.ru>
2023-11-28 21:43:36 +0400

    Добавил краш тест

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,12 @@ app.use(cookieParser());
 
 app.use(reqLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 app.use(
   "/signin",
 
