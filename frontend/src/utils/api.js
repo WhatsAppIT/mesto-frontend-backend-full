2a8162443738e6ba023562b02643c5edd5dff7f1
WhatsAppIT <it.technology@internet.ru>
2023-11-29 20:09:53 +0400

    cors 8

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -105,7 +105,7 @@ class Api {
 const api = new Api({
     url: 'https://api.krivolapov.nomoredomainsmonster.ru',
     headers: {
-        authorization: 'dc5ffe71-d4a2-4ae4-8d8f-113b04708a8c',
+        Authorization: 'dc5ffe71-d4a2-4ae4-8d8f-113b04708a8c',
         'Content-type': 'application/json',
     },
 });
