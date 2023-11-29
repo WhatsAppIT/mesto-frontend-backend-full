0da5b6ef579bb5e64dccad8f73676ea00e37e43c
WhatsAppIT <it.technology@internet.ru>
2023-11-29 19:57:31 +0400

    cors 7

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Origin", "*");
   }
 
   next();
