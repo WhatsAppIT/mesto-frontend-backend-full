54202675f28778fceace8a0b3609e81d3a306717
WhatsAppIT <it.technology@internet.ru>
2023-11-29 11:56:42 +0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { celebrate, Joi, errors } = require("celebrate");
 const { login, postUser } = require("./controllers/users");
@@ -17,6 +18,8 @@ const { PORT = 3002, MONGO_URL = "mongodb://127.0.0.1:27017/mestodb" } =
   process.env;
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
