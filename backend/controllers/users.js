91e3aa0e3d93f81a619bf9be2106b04eac121a3e
WhatsAppIT <it.technology@internet.ru>
2023-11-30 18:26:48 +0400

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,19 +1,17 @@
-const bcrypt = require('bcryptjs');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const { JWT_SECRET } = process.env;
 
-const ValidationError = require('../errors/ValidationError');
-const NotFoundError = require('../errors/NotFoundError');
-const RepetError = require('../errors/RepetError');
-const SigninError = require('../errors/SigninError');
+const ValidationError = require("../errors/ValidationError");
+const NotFoundError = require("../errors/NotFoundError");
+const RepetError = require("../errors/RepetError");
+const SigninError = require("../errors/SigninError");
 
 const postUser = (req, res, next) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
   bcrypt.hash(password, 10).then((hash) => {
     User.create({
@@ -33,15 +31,15 @@ const postUser = (req, res, next) => {
         });
       })
       .catch((err) => {
-        if (err.name === 'ValidationError') {
+        if (err.name === "ValidationError") {
           return next(
             new ValidationError(
-              'Переданы некорректные данные при создании пользователя.',
-            ),
+              "Переданы некорректные данные при создании пользователя."
+            )
           );
         }
         if (err.code === 11000) {
-          return next(new RepetError('Такаой email уже зарегистрирован.'));
+          return next(new RepetError("Такаой email уже зарегистрирован."));
         }
         return next(err);
       });
@@ -52,16 +50,16 @@ const getProfile = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
+        throw new NotFoundError("Нет пользователя с таким id");
       }
       return res.send({ data: user });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === "CastError") {
         return next(
           new ValidationError(
-            'Переданы некорректные данные при поиске пользователя.',
-          ),
+            "Переданы некорректные данные при поиске пользователя."
+          )
         );
       }
 
@@ -73,18 +71,18 @@ const login = (req, res, next) => {
   const { email, password } = req.body;
 
   User.findOne({ email })
-    .select('+password')
+    .select("+password")
     .then((user) => {
       if (!user) {
-        throw new SigninError('Неправильные логин или пароль');
+        throw new SigninError("Неправильные логин или пароль");
       }
 
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          throw new SigninError('Неправильные логин или пароль');
+          throw new SigninError("Неправильные логин или пароль");
         }
         const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-          expiresIn: '7d',
+          expiresIn: "7d",
         });
         res.send({ token });
       });
@@ -109,16 +107,16 @@ const getUserId = async (req, res, next) => {
     const getUser = await User.findById(req.params.userId);
 
     if (!getUser) {
-      throw new NotFoundError('Нет пользователя с таким id');
+      throw new NotFoundError("Нет пользователя с таким id");
     }
 
     return res.send(getUser);
   } catch (err) {
-    if (err.name === 'CastError') {
+    if (err.name === "CastError") {
       return next(
         new ValidationError(
-          'Переданы некорректные данные при поиске пользователя.',
-        ),
+          "Переданы некорректные данные при поиске пользователя."
+        )
       );
     }
 
@@ -135,20 +133,20 @@ const patchUsersMe = async (req, res, next) => {
         name,
         about,
       },
-      { new: true, runValidators: true },
+      { new: true, runValidators: true }
     );
 
     if (!patchUser) {
-      throw new NotFoundError('Пользователь по указанному _id не найден.');
+      throw new NotFoundError("Пользователь по указанному _id не найден.");
     }
 
     return res.send(patchUser);
   } catch (err) {
-    if (err.name === 'ValidationError') {
+    if (err.name === "ValidationError") {
       return next(
         new ValidationError(
-          'Переданы некорректные данные при поиске пользователя.',
-        ),
+          "Переданы некорректные данные при поиске пользователя."
+        )
       );
     }
 
@@ -162,20 +160,20 @@ const patchUsersMeAvatar = async (req, res, next) => {
     const patchAvatar = await User.findByIdAndUpdate(
       req.user._id,
       { avatar },
-      { new: true, runValidators: true },
+      { new: true, runValidators: true }
     );
 
     if (!patchAvatar) {
-      throw new NotFoundError('Аватар по указанному _id не найден.');
+      throw new NotFoundError("Аватар по указанному _id не найден.");
     }
 
     return res.send(patchAvatar);
   } catch (err) {
-    if (err.name === 'ValidationError') {
+    if (err.name === "ValidationError") {
       return next(
         new ValidationError(
-          'Переданы некорректные данные при поиске пользователя.',
-        ),
+          "Переданы некорректные данные при поиске пользователя."
+        )
       );
     }
 
