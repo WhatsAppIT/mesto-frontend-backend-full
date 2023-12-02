a470a54968973e22b2d20f0bd9cbc5eb18ac9a01
WhatsAppIT <it.technology@internet.ru>
2023-12-02 17:38:35 +0400

    asasasdasdasdasdxas

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    throw new SigninError("Необходима авторизация 777");
+    throw new SigninError("Необходима авторизация 777888");
   }
 
   const token = authorization.replace("Bearer ", "");
