4f42d3901ea0873bf760e20882d0d2aa0bf028de
WhatsAppIT <it.technology@internet.ru>
2023-12-03 22:01:01 +0400

    77777777777777777

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,36 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const { celebrate, Joi, errors } = require("celebrate");
-const { login, postUser } = require("./controllers/users");
-const { reqLogger, errorLogger } = require("./middlewares/logger");
-const auth = require("./middlewares/auth");
-const errorHandler = require("./middlewares/error-handler");
-const routerUsers = require("./routes/users");
-const routerCards = require("./routes/cards");
-const NotFoundError = require("./errors/NotFoundError");
-const { linkRegex } = require("./utils/constants");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const { login, postUser } = require('./controllers/users');
+const { reqLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/error-handler');
+const routerUsers = require('./routes/users');
+const routerCards = require('./routes/cards');
+const NotFoundError = require('./errors/NotFoundError');
+const { linkRegex } = require('./utils/constants');
 
-const { PORT = 3000, MONGO_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
 app.use(
   cors({
     origin: [
-      "https://krivolapov.nomoredomainsmonster.ru",
-      "http://krivolapov.nomoredomainsmonster.ru",
-      "http://localhost:3000",
-      "https://api.krivolapov.nomoredomainsmonster.ru",
-      "http://api.krivolapov.nomoredomainsmonster.ru",
-      "http://localhost:3001",
+      'https://krivolapov.nomoredomainsmonster.ru',
+      'http://krivolapov.nomoredomainsmonster.ru',
+      'http://localhost:3000',
+      'https://api.krivolapov.nomoredomainsmonster.ru',
+      'http://api.krivolapov.nomoredomainsmonster.ru',
+      'http://localhost:3001',
     ],
-    credentials: "include",
+    credentials: 'include',
     maxAge: 30,
-  })
+  }),
 );
 
 app.use(bodyParser.json());
@@ -40,24 +39,24 @@ app.use(cookieParser());
 
 app.use(reqLogger);
 
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 
 app.use(
-  "/signin",
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
-  login
+  login,
 );
 app.use(
-  "/signup",
+  '/signup',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
@@ -67,16 +66,12 @@ app.use(
       password: Joi.string().required(),
     }),
   }),
-  postUser
+  postUser,
 );
 app.use(auth);
-app.use("/users", routerUsers);
-app.use("/cards", routerCards);
-
-app.use("*", (req, res, next) =>
-  next(new NotFoundError("Страница не найдена"))
-);
-
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
+app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 app.use(errorLogger);
 app.use(errors());
 
@@ -84,9 +79,7 @@ app.use(errorHandler);
 
 async function init() {
   await mongoose.connect(MONGO_URL);
-  console.log(MONGO_URL);
   await app.listen(PORT);
-  console.log(PORT);
 }
 
 init();
