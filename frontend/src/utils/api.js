acfe16d2301170aaa99e37b6fa3b8f6100a8de59
WhatsAppIT <it.technology@internet.ru>
2023-12-04 14:16:07 +0400

    1111111111

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -114,7 +114,7 @@ class Api {
 const api = new Api({
   url: "https://api.krivolapov.nomoredomainsmonster.ru",
   headers: {
-    authorization: `Bearer ${localStorage.getItem('jwt')}`,
+    //authorization: `Bearer ${localStorage.getItem('jwt')}`,
     "Content-type": "application/json",
   },
 });
