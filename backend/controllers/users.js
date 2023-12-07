a6a221541bb7295fdfb944621ef032d6227c7e40
WhatsAppIT <it.technology@internet.ru>
2023-12-07 20:28:33 +0400

    aaaaaaaaaaaa

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -81,7 +81,13 @@ const login = (req, res, next) => {
         const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
           expiresIn: '7d',
         });
-        res.send({ token });
+        res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        });
+        res.send({ message: 'Вы успешно авторизованы' });
       })
       .catch((err) => {
         next(err);
