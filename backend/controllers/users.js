e8fa964693d791f278557e318b71c87545c0eb30
WhatsAppIT <it.technology@internet.ru>
2023-12-09 00:15:34 +0400

    zzzzzzzzzzzz

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -87,7 +87,7 @@ const login = (req, res, next) => {
         if (!matched) {
           throw new SigninError('Неправильные логин или пароль c u login');
         }
-        const token = jwt.sign(user._id, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
+        const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
           expiresIn: '7d',
         });
         res.send({ token });
